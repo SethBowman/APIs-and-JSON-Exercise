20118c7a3b5f828896aef4e071f84128f0ca9af5
Seth Bowman <brandonsethbowman@gmail.com>
2023-06-16 14:00:31 -0500

    Exercise 1 completed

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public static class RonVSKanyeAPI
     {
+        public static void Convo()
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine($"Ron says:\n{GetSwansonQuote(client)}\n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Kanye says:\n{GetKanyeQuote(client)}\n");
+                Thread.Sleep(2000);
+            }
+        }
+        private static string GetSwansonQuote(HttpClient client)
+        {
+            var jText = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+            var quote = JArray.Parse(jText).ToString().Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            return quote;
+        }
+
+        private static string GetKanyeQuote(HttpClient client) 
+        {
+            var jText = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            var quote = JObject.Parse(jText).GetValue("quote").ToString();
+
+            return quote;
+        }
     }
 }
